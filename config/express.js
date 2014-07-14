c3b4c30b625d8fd76fd5ed5a67bac58e87fec38e
Dca <dca35311@gmail.com>
2014-07-14 15:15:26 +0800

    升級到 express 4.x

--- config/express.js
+++ config/express.js
@@ -1,32 +1,37 @@
 var express = require('express');
+var compression = require('compression');
+var lessMiddleware = require('less-middleware');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
 
 module.exports = function(app, config) {
 
-    app.configure(function() {
-        app.use(express.compress());
-        console.log(config.root);
-        app.use(require('less-middleware')({
-            paths   : [ config.root + '/src/components/bootstrap/less'],
-            src     : config.root + '/src/less',
-            dest    : config.root + '/public/stylesheets',
-            prefix  : '/stylesheets',
-            compress: true
-        }));
-        app.use(express.static(config.root + '/public'));
-        app.set('port', config.port);
-        app.set('views', config.root + '/app/views');
-        app.set('view engine', 'jade');
-        // app.use(express.favicon(config.root + '/public/img/favicon.ico'));
-        app.use(express.logger('dev'));
-        app.use(express.bodyParser());
-        app.use(express.methodOverride());
-        app.use(express.cookieParser('your secret here'));
-        app.use(express.session({ secret: 'keyboard cat' }));
+    app.use(compression());
+    console.log(config.root);
+    app.use(lessMiddleware(config.root + '/src/less',{
+        paths   : [ config.root + '/src/components/bootstrap/less'],
+        src     : config.root + '/src/less',
+        dest    : config.root + '/public/stylesheets',
+        prefix  : '/stylesheets',
+        compress: true
+    }));
+    app.use(express.static(config.root + '/public'));
+    app.set('port', config.port);
+    app.set('views', config.root + '/app/views');
+    app.set('view engine', 'jade');
+    // app.use(express.favicon(config.root + '/public/img/favicon.ico'));
+    // app.use(express.logger('dev'));
+    app.use(bodyParser());
+    app.use(methodOverride());
+    app.use(cookieParser('your secret here'));
+    app.use(session({ secret: 'keyboard cat' }));
 
-        app.use(app.router);
+    // app.use(app.router);
 
 
-    });
 
     // development only
     if ('development' == app.get('env')) {
